05bcb8ed311b07bbd9553531498250b1c598c1a7
AlvinSaini <67993125+AlvinSaini@users.noreply.github.com>
2021-09-30 10:55:30 +0530

    fix the error of stoping in between

--- main.py
+++ main.py
@@ -1,196 +1,111 @@
 #=======imports=======#
 
-
-
 from tkinter import *
-
 from tkinter.colorchooser import askcolor
-
-
-
 
 
 root=Tk()
 
-
-
 ## variable defining####
-
-colour=choice(["blue","black","red","green","yellow","grey","orange"])
-
-
-
-#======colours selector======#
-
-def changeBg():
-
-        (triple, hexstr) = askcolor()
-
-        if hexstr:
-
-         canvas.config(background=hexstr)
-
+colour="blue"
 
 #========Theme Bacground========#
+def changeBg():
+	   (triple, hexstr) = askcolor()
+	   if hexstr:
+	   	canvas.config(background=hexstr)
+
 def Light():
-
-        root.title("Alvin's Whiteboard")
-
-        canvas.config(background="white")
-
+    root.title("Alvin's Whiteboard")
+    canvas.config(background="white")
         
-
 def Dark():
-
-     root.title("Alvin's blackboard")
-
-     canvas.config(background="black")
-
-
+	  root.title("Alvin's blackboard")
+	  canvas.config(background="black")
 
 #========fg colour===========#
-
 def changeFg():
-
-        global colour
-
-        (triple, hexstr) = askcolor()
-
-        if hexstr:
-
-         colour=hexstr
-
-
+	  global colour
+	  (triple, hexstr) = askcolor()
+	  if hexstr:
+	   	colour=hexstr
 
 def blue():
-
- global colour
-
- colour="blue"
-
+	global colour
+	colour="blue"
 def white():
-
- global colour
-
- colour="white"
-
+	global colour
+	colour="white"
 def red():
-
- global colour
-
- colour="red"
-
+	global colour
+	colour="red"
 def black():
-
- global colour
-
- colour="black"
-
-                    
-
+	global colour
+	colour="black"
+			  	       	     	
 #=======line creating=========#
-
+#x0,y0=0,0
 def paint(event, selected_colour):
+    '''Draws a line following the user mouse cursor'''
+    global x0,y0
+    x1,y1=event.x,event.y
+    if x0!=0 and y0!=0:
+    	canvas.create_line(x0,y0,x1,y1, fill=selected_colour, width=s1.get(),capstyle=ROUND,smooth=True)
+    x0,y0=x1,y1
 
-    '''Draws a line following the user mouse cursor'''        
+def setZero():
+	global x0,y0
+	x0,y0=0,0
 
-    x1,y1=event.x+1,event.y+1
-
-    x2, y2 = event.x-2, event.y-2
-
-    canvas.create_line(x1,y1,x2,y2, fill=selected_colour, width=s1.get(),capstyle=ROUND,smooth=True)
-
-    
 # basic size and name of GUI
-
-can_width=1100
-
-can_height=1800
-
+can_width=1040
+can_height=2160
 root.geometry(f"{can_width}x{can_height}")
-
 root.minsize(width=500,height=500)
-
 root.title("Alvin's board")
 
-
-
-#======width scale=====#
-
-s1=Scale(root,from_=4,to=50,orient=HORIZONTAL)
-
+#======width  scale=====#
+s1=Scale(root,from_=8,to=25,orient=HORIZONTAL)
 s1.pack(anchor="n",ipadx=840,side="top")
 
 
 #========creating Canvas===========#
-
-canvas = Canvas(root,width=can_width,height=can_height, background='black')
-
+canvas = Canvas(root,width=can_width,height=can_height, background='black',cursor="dot")
 canvas.pack(fill="both")
-
-canvas.bind("<Motion>", lambda event: paint(event,selected_colour=colour))
-
-
+canvas.bind("<B1-Motion>", lambda event: paint(event,selected_colour=colour))
+canvas.bind("<ButtonRelease-1>",lambda event: setZero())
 
 #=========creating main menu=========#
-
 mainMenu=Menu(root)
 
 
 #========creating sub menus==========#
-
 m1=Menu(mainMenu)
-
 m2=Menu(mainMenu)
-
 m3=Menu(mainMenu)
 
-
-
 mainMenu.add_cascade(label="Colour",menu=m1)
-
 mainMenu.add_cascade(label="Theme",menu=m2)
-
 mainMenu.add_cascade(label="Erase",menu=m3)
 
-
-
 #====adding command in menu m1=====#
-
-m1.add_command(label=" ",background="white",command=white)
-
-m1.add_command(label=" ",background="blue",command=blue)
-
-m1.add_command(label=" ",background="red",command=red)
-
-m1.add_command(label=" ",background="black",command=black)
-
+m1.add_command(label="        ",background="white",command=white)
+m1.add_command(label="        ",background="blue",command=blue)
+m1.add_command(label="        ",background="red",command=red)
+m1.add_command(label="        ",background="black",command=black)
 m1.add_command(label="Other",command=changeFg)
 
-
-
 #====adding command in menu m2=====#
-
-m2.add_command(label=" ",background="white",command=lambda : canvas.config(background="white"))
-
-m2.add_command(label=" ",background="black",command=lambda : canvas.config(background="black"))
-
-m2.add_comment(label=" ", background="blue", command=lambda : canvas.config(background="blue"))
-
+m2.add_command(label="       ",background="white",command=lambda : canvas.config(background="white"))
+m2.add_command(label="       ",background="black",command=lambda : canvas.config(background="black"))
 m2.add_command(label="Other",command=changeBg)
 
-
-
 #======adding command in menu m3====#
-
 m3.add_command(label="Clear canvas",command=lambda: canvas.delete("all"))
-m3.add_command(label="Erase", command= lambda event : canvas.delete(event.x,event.y)
-
 
 #=========configing Menu===========#
-
 root.config(menu=mainMenu)
 
-#========running mainloop========#
 
+#========running mainloop========#
 root.mainloop()
